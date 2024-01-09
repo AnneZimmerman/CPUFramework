6b98ad926e2e2f255e473d68d346c6d8957e5703
AnneZimmerman <annezimmerman@mailstack.com>
2024-01-09 09:35:10 -0500

    initial commit

---
+++ SQLUtility.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace CPUFramework
+{
+    public class SQLUtility
+    {
+
+        public static string ConnectionString = "";
+        
+        public static DataTable GetDataTable(string sqlstatement) // - take a SQL statemetn and return a DataTable
+        {
+            Debug.Print(sqlstatement);
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = ConnectionString;
+            conn.Open();
+            var cmd = new SqlCommand();
+            cmd.CommandText = sqlstatement;
+            cmd.Connection = conn;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
